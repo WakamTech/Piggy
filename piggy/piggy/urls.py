99654be14ae0c8b0748ef6b56db8bc5ecbbf4866
WakamTech <williammerveilleaklamavo@gmail.com>
2024-07-11 07:49:03 +0100

    ok

--- piggy/piggy/urls.py
+++ piggy/piggy/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('marketplace.urls')),
+    path('api/', include('marketplace.urls')),
 ]
